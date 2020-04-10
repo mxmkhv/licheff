8c1226f90c00a919344455fbf9f02bf0f95ad90d
maxkehayov <maxkehayov@gmail.com>
2020-04-10 10:37:51 +0300

    l

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,15 +1,28 @@
 // exports.createPages = async ({ actions, graphql, reporter }) => {
 //   const result = await graphql(`
-//     {
+//     query {
 //       allMarkdownRemark {
-//         edges {
-//           node {
-//             frontmatter {
-//               title
-//             }
+//         nodes {
+//           frontmatter {
+//             project_title
 //           }
 //         }
 //       }
 //     }
 //   `);
+
+//   if (result.errors) {
+//     reporter.panicOnBuild(`Error while running GraphQL query.`);
+//     return;
+//   }
+
+//   const projects = result.data.allMarkdownRemark.nodes;
+
+//   projects.forEach(({ project }) => {
+//     createPage({
+//       path: project.frontmatter.project_title,
+//       component: blogPostTemplate,
+//       context: {} // additional data can be passed via context
+//     });
+//   });
 // };
