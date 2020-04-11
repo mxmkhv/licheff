6b372c2a3c5cf48a56b1ec84b1fa26b178cdeb56
maxkehayov <maxkehayov@gmail.com>
2020-04-11 11:44:42 +0300

    resume typography

--- src/components/ContactLink/ContactLink.js
+++ src/components/ContactLink/ContactLink.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import styles from './ContactLink.module.scss';
 
-const BackLink = props => {
+const BackLink = () => {
   return (
     <div className={styles.link}>
-      <a href='mailto:someone@yoursite.com'>
+      <a href='mailto:licheff.o@gmail.com'>
         <span data-hover='licheff.o@gmail.com'>Get in touch @</span>
       </a>
     </div>
