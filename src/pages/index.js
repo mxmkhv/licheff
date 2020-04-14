fb2956718450bd5a10d800ee7f9b85c62d22f7e1
maxkehayov <maxkehayov@gmail.com>
2020-04-14 20:42:06 +0300

    scroll top index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AniLink from 'gatsby-plugin-transition-link/AniLink';
 import Layout from '../components/Layout/Layout';
 import SEO from '../components/seo';
@@ -11,35 +11,42 @@ import Logo from '../components/Logo/Logo';
 import ContactLink from '../components/ContactLink/ContactLink';
 import ProjectListing from '../components/ProjectListing/ProjectListing';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title='Hello' />
-    <InteractionPanel>
-      <Logo />
-      <div style={{ marginTop: 'auto' }}>
-        <Label size='2.2em'>Todor Lichev</Label>
-        <Label size='1.5em' color='red'>
-          Designer
-        </Label>
-        <BodyText>
-          A young multidisciplinary artist with a solid background in User Experience/Interfaces,
-          Branding &amp; WEB Design. The dynamics of progress and new challenges developed a keen
-          interest in the field of brand perceptions. A self-driven hard worker and a passionate
-          freethinker who is always looking forward to expand their worldview.
-        </BodyText>
-        <div className='resume-link'>
-          <AniLink to='/resume/' cover direction='left' bg='var(--red)' duration={1}>
-            View resume
-          </AniLink>
+const IndexPage = () => {
+  useEffect(() => {
+    const screen = document.getElementById('interactionPanel');
+    screen.scrollIntoView(true);
+  }, []);
+
+  return (
+    <Layout>
+      <SEO title='Hello' />
+      <InteractionPanel>
+        <Logo />
+        <div style={{ marginTop: 'auto' }}>
+          <Label size='2.2em'>Todor Lichev</Label>
+          <Label size='1.5em' color='red'>
+            Designer
+          </Label>
+          <BodyText>
+            A young multidisciplinary artist with a solid background in User Experience/Interfaces,
+            Branding &amp; WEB Design. The dynamics of progress and new challenges developed a keen
+            interest in the field of brand perceptions. A self-driven hard worker and a passionate
+            freethinker who is always looking forward to expand their worldview.
+          </BodyText>
+          <div className='resume-link'>
+            <AniLink to='/resume/' cover direction='left' bg='var(--red)' duration={1}>
+              View resume
+            </AniLink>
+          </div>
+          <ContactLink />
+          <SocialLinks />
         </div>
-        <ContactLink />
-        <SocialLinks />
-      </div>
-    </InteractionPanel>
-    <Content>
-      <ProjectListing />
-    </Content>
-  </Layout>
-);
+      </InteractionPanel>
+      <Content>
+        <ProjectListing />
+      </Content>
+    </Layout>
+  );
+};
 
 export default IndexPage;
