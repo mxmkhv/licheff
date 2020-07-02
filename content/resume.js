5363be9ffc566cfb661940900f0c74793963f57b
maxkehayov <maxkehayov@gmail.com>
2020-07-02 17:04:45 +0300

    resume typo fix

--- content/resume.js
+++ content/resume.js
@@ -2,11 +2,14 @@ const resume = {
   title: 'Resume',
   subtitle: 'WORK EXPERIENCE',
   positions: [
-     {
+    {
       role: 'Freelance Designer',
       period: 'January 2018 - Present',
       description: [
-        { text: 'Complete freelance projects for international clients in fields ranging from real estate (Konstruktive.bg), to non-profit organisations (365 Association), retail (Popertee), agencies (Smartinteractive)' },
+        {
+          text:
+            'Complete freelance projects for international clients in fields ranging from real estate (Konstruktive.bg), to non-profit organisations (365 Association), retail (Popertee), agencies (Smartinteractive)'
+        },
         { text: 'Regular client meetings, both online and offline' },
         { text: 'Create logo designs, brand identities and print materials' }
       ]
@@ -17,7 +20,10 @@ const resume = {
       period: 'May 2019 - Present',
       description: [
         { text: 'Taking a key role in creating a complete design system for all company products' },
-        { text: 'Mainly working on improving the user experience of the mobile application (iOS & Android)' },
+        {
+          text:
+            'Mainly working on improving the user experience of the mobile application (iOS & Android)'
+        },
         { text: 'Conducting internal and external usability tests' }
       ]
     },
@@ -39,7 +45,10 @@ const resume = {
         { text: 'Project management and schedule planning' },
         { text: 'Allocation of tasks and delegating customer feedback' },
         { text: 'Regular client meetings to present projects' },
-        { text: 'Key role in the design of a new Prodesign brand identity and development of a brandbook' }
+        {
+          text:
+            'Key role in the design of a new Prodesign brand identity and development of a brandbook'
+        }
       ]
     },
     {
@@ -47,10 +56,18 @@ const resume = {
       company: 'Prodesign Bulgaria',
       period: 'October 2014 – October 2016',
       description: [
-        { text: 'Brand Identity - brand research, brand positioning, naming, sketching, concept presentations, brand implementation, print production' },
-        { text: 'Print Production – different printing technologies knowledge development, print products preparation, design of attractive print products' },
-        { text: 'Digital – design of mobile apps, responsive websites, UX/UI, mindmapping, sitemaps' },
-        { text: '3D Package design of bottles, boxes and exhibition stands' }
+        {
+          text:
+            'Brand Identity - brand research, brand positioning, naming, sketching, concept presentations, brand implementation, print production'
+        },
+        {
+          text:
+            'Print Production – different printing technologies knowledge development, print products preparation, design of attractive print products'
+        },
+        {
+          text: 'Digital – design of mobile apps, responsive websites, UX/UI, mindmapping, sitemaps'
+        },
+        { text: '3D Package design of bottles, boxes and exhibition stands' },
         { text: 'Website Development – design and basic programming skills' }
       ]
     },
@@ -59,7 +76,9 @@ const resume = {
       company: 'Prodesign Bulgaria',
       period: 'April 2014 – September 2014',
       description: [
-        { text: 'Design and implementation of a new file structuring scheme and file storage system' },
+        {
+          text: 'Design and implementation of a new file structuring scheme and file storage system'
+        }
       ]
     }
   ]
